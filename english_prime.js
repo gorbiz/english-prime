2e81b1f47fe52668380a5a1b99c79f945f469a66
karlsvartholm <gorbiz@gmail.com>
2011-01-14 00:33:47 +0100

    Fixed a bug by removing a reference to a none existing variable

--- english_prime.js
+++ english_prime.js
@@ -46,7 +46,7 @@ function english_prime_helper() {
 	var score = cleaned_text.length / forbidden_words_found.length;
 
 	/* The style is sort of a hack added for the bookmarklet */
-	var html = '<p style="display: block; position: fixed;top: 0;left: 0;color: #999;background: #eee;padding: 0.5em;z-index:10000;" class="' + cssclass + '"><strong>Score:</strong> ' + score + '<br />'
+	var html = '<p style="display: block; position: fixed;top: 0;left: 0;color: #999;background: #eee;padding: 0.5em;z-index:10000;"><strong>Score:</strong> ' + score + '<br />'
 			+ '<strong>Found ' + forbidden_words_found.length + ' forbidden words out of ' + cleaned_text.length + ' words:</strong> ' + forbidden_words_found + '</p>';
 
 	if (! document.getElementById('english_prime_status')) {
@@ -58,7 +58,7 @@ function english_prime_helper() {
 	document.getElementById('english_prime_status').innerHTML = html;
 }
 
-english_prime_helper();
+//english_prime_helper();
 
 // Bookmarklet, tested in Chrome
 // javascript:function create_english_prime_helper(){var oldonload=window.onload;if(typeof window.onload!='function'){window.onload=english_prime_helper;}else{window.onload=function(){if(oldonload){oldonload();}english_prime_helper();}}}function english_prime_helper(){var tolerance=3;var forbidden_words=["be","is","am","are","was","were","been","being"];var forbidden_strings=["'s","'m","'re"];var cleaned_text=document.body.innerText.toLowerCase().replace(/[\.\,\"\:\;\?\-\!]/ig,' ').split(' ');var forbidden_words_found=[];for(var i in cleaned_text){if(forbidden_words.indexOf(cleaned_text[i])>-1){forbidden_words_found.push(cleaned_text[i]);}for(var j in forbidden_strings){if(cleaned_text[i].indexOf(forbidden_strings[j])>-1){forbidden_words_found.push(cleaned_text[i]);}}}var score=cleaned_text.length/forbidden_words_found.length;var cssclass="ok";if(forbidden_words_found.length>tolerance){cssclass="error";}/*The style is sort of a hack added for the bookmarklet*/var html='<p style="display:block;position:fixed;top:0;left:0;color:#999;background:#eee;padding:0.5em;z-index:10000;"class="'+cssclass+'"><strong>Score: </strong>'+score+'<br/>'+'<strong>Found  '+forbidden_words_found.length+' forbidden words out of '+cleaned_text.length+' words: </strong>'+forbidden_words_found+'</p>';if(!document.getElementById('english_prime_status')){var english_prime_status=document.createElement('div');english_prime_status.setAttribute('id','english_prime_status');english_prime_status.innerHTML=html;document.body.insertBefore(english_prime_status,document.body.firstChild);}document.getElementById('english_prime_status').innerHTML=html;}english_prime_helper();
